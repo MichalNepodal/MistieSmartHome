871d98b1b6748ad5cebb6580290b13418049c197
MichalNepodal <michal.nepodal.it@email.cz>
2021-02-01 00:34:46 +0100

    Úprava SpravceHudby

--- Mistie_v_3/BackEnd/SpravceHudby.cs
+++ Mistie_v_3/BackEnd/SpravceHudby.cs
@@ -13,7 +13,8 @@ namespace Mistie_v_3
     public class SpravceHudby
     {
         SpravceZabezpeceni spravceZabezpeceni;
-
+        Random random = new Random();
+        SoundPlayer soundPlayer;
 
         public SpravceHudby(SpravceZabezpeceni spravceZabezpeceni)
         {
@@ -22,132 +23,122 @@ namespace Mistie_v_3
         }
 
 
-        Random random = new Random();
-
-        SoundPlayer soundPlayer;
-
-
-        // POUŠTĚŤNÍ HUDBY          POUŠTĚŤNÍ HUDBY        POUŠTĚŤNÍ HUDBY             POUŠTĚŤNÍ HUDBY
-        // POUŠTĚŤNÍ HUDBY          POUŠTĚŤNÍ HUDBY        POUŠTĚŤNÍ HUDBY             POUŠTĚŤNÍ HUDBY
-        // POUŠTĚŤNÍ HUDBY          POUŠTĚŤNÍ HUDBY        POUŠTĚŤNÍ HUDBY             POUŠTĚŤNÍ HUDBY
+        // SPUŠTĚNÍ HUDBY
+        // SPUŠTĚNÍ HUDBY
 
         public void StopMusic()
         {
             if (soundPlayer != null)
                 soundPlayer.Stop();                
         }
-
         public void PlayMusic()
         {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Music\Music1.wav");
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Music\music1.wav");
             soundPlayer.Play();
         }
 
+        // HLASOVÉ ODPOVĚDI
+        // HLASOVÉ ODPOVĚDI
 
-        // ODPOVĚDI          ODPOVĚDI            ODPOVĚDI                 ODPOVĚDI                 ODPOVĚDI
-        // ODPOVĚDI          ODPOVĚDI            ODPOVĚDI                 ODPOVĚDI                 ODPOVĚDI
-        // ODPOVĚDI          ODPOVĚDI            ODPOVĚDI                 ODPOVĚDI                 ODPOVĚDI
-
-        public void PrichodOdemceni()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\OdemcenoPrichod1.wav");
-            soundPlayer.Play();
-        }
-        public void OdchodUzamceni()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\UzamykamOdchod1.wav");
-            soundPlayer.Play();
-        }
-        public void Deaktivace()
-        {            
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Deaktivace1.wav");
-            soundPlayer.Play();
-        }
-        public void AktivaceVnejsiOchrana()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\AktivaceVnesjiZabezpeceni1.wav");
-            soundPlayer.Play();
-        }
-        public void AktivaceRezimSpanek()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\AktivaceRezimSpanek1.wav");
-            soundPlayer.Play();
-        }
-        public void DeaktivaceBezpecnostniSystem()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\DeaktivaceBezpecnostniSystem1.wav");
-            soundPlayer.Play();
-        }
-        public void AktivaceBezpecnostniSystem()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\AktivaceBezpecnostniSystem1.wav");
-            soundPlayer.Play();
-        }
-
-        public void ZvukNespravnehoHesla()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_5 - 15.1.2021 - organizace složek\Mistie_v_3\Sounds\spatneHeslo1.wav");
-            soundPlayer.Play();
-        }
-        public void ZvukSpravnehoHesla()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_5 - 15.1.2021 - organizace složek\Mistie_v_3\Sounds\spravneHeslo1.wav");
-            soundPlayer.Play();
-        }
-        public void ZvukAlarm1()
-        {
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_5 - 15.1.2021 - organizace složek\Mistie_v_3\Sounds\alarm1.wav");
-            soundPlayer.Play();
-        }
         public void PlaySayYes()
         {
             if (NahodneCislo() == 0)
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Jo.wav");
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Jo.wav");
             else
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Jo2.wav");
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Jo2.wav");
             soundPlayer.Play();
         }
         public void PlaySayHudba()
         {
             if (NahodneCislo() == 0)
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\PoustimHudbu.wav");
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\PoustimHudbu.wav");
             else
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\PoustimHudbu2.wav");
-            soundPlayer.Play();            
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\PoustimHudbu2.wav");
+            soundPlayer.Play();
             Thread.Sleep(1500);
         }
         public void SayLightsOn()
         {
             if (NahodneCislo() == 0)
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Rozsvicuji.wav");
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Rozsvicuji.wav");
             else
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Rozsvicuji2.wav");
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Rozsvicuji2.wav");
             soundPlayer.Play();
             Thread.Sleep(1000);
         }
         public void SayLightsOff()
         {
             if (NahodneCislo() == 0)
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Zhasinam.wav");
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Zhasinam.wav");
             else
-                soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Zhasinam2.wav");
+                soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Zhasinam2.wav");
             soundPlayer.Play();
             Thread.Sleep(1000);
         }
         public void SayNothing()
-        {            
-            soundPlayer = new SoundPlayer(@"D:\C# učím se programovat 8.2020\WPF Aplikace (windows)\Mistie_v_3\Mistie_v_3\Sounds\Nic1.wav");
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Nic1.wav");
             soundPlayer.Play();
             Thread.Sleep(1000);
         }
+        public void PrichodOdemceni()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\OdemcenoPrichod1.wav");
+            soundPlayer.Play();
+        }
+        public void OdchodUzamceni()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\UzamykamOdchod1.wav");
+            soundPlayer.Play();
+        }
+        public void Deaktivace()
+        {            
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\Deaktivace1.wav");
+            soundPlayer.Play();
+        }
+        public void AktivaceVnejsiOchrana()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\AktivaceVnesjiZabezpeceni1.wav");
+            soundPlayer.Play();
+        }
+        public void AktivaceRezimSpanek()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\AktivaceRezimSpanek1.wav");
+            soundPlayer.Play();
+        }
+        public void DeaktivaceBezpecnostniSystem()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\DeaktivaceBezpecnostniSystem1.wav");
+            soundPlayer.Play();
+        }
+        public void AktivaceBezpecnostniSystem()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\AktivaceBezpecnostniSystem1.wav");
+            soundPlayer.Play();
+        }
 
+        public void ZvukNespravnehoHesla()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\spatneHeslo1.wav");
+            soundPlayer.Play();
+        }
+        public void ZvukSpravnehoHesla()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\spravneHeslo1.wav");
+            soundPlayer.Play();
+        }
+        public void ZvukAlarm1()
+        {
+            soundPlayer = new SoundPlayer(@"D:\MistieSmartHome\Mistie_v_0.07_31_1_2021\Mistie_v_3\Sounds\alarm1.wav");
+            soundPlayer.Play();
+        }
 
+        // INTERNÍ METODY 
+        // INTERNÍ METODY 
         private int NahodneCislo()
         {
             return random.Next(2);
         }
-
-
 
         // METODA PRI VYTVORENI
 
@@ -156,6 +147,4 @@ namespace Mistie_v_3
             spravceZabezpeceni.NactiSpravceHudby(this);
         }
     }
-
-    
 }
